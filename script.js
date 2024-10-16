f000f27e740f7e2f77b77441fd6551af130f53de
ShadowGuy01234 <121357713+ShadowGuy01234@users.noreply.github.com>
2024-10-16 20:32:32 +0530

    made color change on hover

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+function colorOnClick() {
+    document.addEventListener("DOMContentLoaded", ()=> {
+        let grid = document.querySelector(".grid");
+        grid.addEventListener("click", () => {
+            grid.style.backgroundColor = "aqua";
+        })
+    });
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-    // let n = prompt("Choose grid: ");
-    const canvas = document.querySelector(".canvas");
-    for(let i = 0; i < n; i++) {
-        let newDiv = document.createElement("div");
-        for (let j = 0; j < n; j++) {
-            let div = document.createElement(`div${i}`);
-            div.classList.add("grid");
-            newDiv.appendChild(div, {i});
+
+function grid() {
+    document.addEventListener("DOMContentLoaded", () => {
+        // let n = prompt("Choose grid: ");
+        const canvas = document.querySelector(".canvas");
+        for(let i = 0; i < 3; i++) {
+            let newDiv = document.createElement("div");
+            for (let j = 0; j < 3; j++) {
+                let div = document.createElement(`div`);
+                div.classList.add("grid");
+                newDiv.appendChild(div);
+            }
+            canvas.appendChild(newDiv);
         }
-        canvas.appendChild(newDiv);
-    }
-});
+    });
+}
+
+grid();
+colorOnClick();
